704316ba5ac6a64ebedd00e31f4b5a3a60532bbd
maximilianodelgado <47453544+maximilianodelgado@users.noreply.github.com>
2019-02-08 18:25:00 -0300

    Ajustes del front llamando api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './Logo-Banco-Galicia.svg';
 import './App.css';
 
+const API = 'http://localhost:3001/poc-api/users';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+         (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-         
-        </header>
-      </div>
-    );
+
+    const { error, isLoaded, items } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="container-fluid App">
+
+          <div className="col-md-2">
+            <div className="row">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+              </header>
+            </div>
+          </div>
+
+          <div className="col-md-10">
+            <div className="row text-left">
+              <div className="container-fluid">
+
+                <ul>
+                  {items.map(item => (
+                    <li key={item.id}>
+                      {item.id} {item.username}
+                    </li>
+                  ))}
+                </ul>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+      );
+    }
   }
 }
 
